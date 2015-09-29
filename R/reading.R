25274a6da4e9f7c9c8b2bcb7a858a113cb9e9b6f
Mikhail Popov <mikhail@mpopov.com>
2015-09-29 12:46:51 -0700

    Allows user to pass arguments to read_delim This is needed in case readr::read_delim auto-detects certain columns to be of the wrong type (e.g. it only sees integers in the first few rows but there are doubles later in the dataset). This way we can tell it the column types explicitly if needed.
    Change-Id: Id02522771cc0b6b332c5e1e9699ecba2c0c166fa

--- R/reading.R
+++ R/reading.R
@@ -3,12 +3,13 @@
 #'\href{https://datasets.wikimedia.org}{datasets.wikimedia.org} for use in dashboarding.
 #'
 #'@param path the path to the file, starting from datasets.wikimedia.org/aggregate-datasets/
+#'@param ... Additional arguments to be passed to \code{readr::read_delim}
 #'
 #'@importFrom readr read_delim
 #'@export
-read_dataset <- function(path){
+read_dataset <- function(path, ...){
   location <- paste0("http://datasets.wikimedia.org/aggregate-datasets/", path,
                      "?ts=", gsub(x = Sys.time(), pattern = "(-| )", replacement = ""))
   con <- url(location)
-  return(readr::read_delim(con, delim = "\t"))
+  return(readr::read_delim(con, delim = "\t", ...))
 }
